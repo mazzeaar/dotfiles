786b861a9d7dded0379a48c8679ff89519efa882
amazzetta <aaron.mazzetta@gmail.com>
2025-04-04 16:21:57 +0200

    explicitly disabling annoying color annotations

--- nvim/.config/nvim/lua/plugins/nvim_highlight_colors.lua
+++ nvim/.config/nvim/lua/plugins/nvim_highlight_colors.lua
@@ -10,16 +10,16 @@ return {
 			render = "background",
 
 			---Highlight hex colors, e.g. '#FFFFFF'
-			-- enable_hex = true,
+			enable_hex = false,
 
 			---Highlight short hex colors e.g. '#fff'
-			-- enable_short_hex = true,
+			enable_short_hex = false,
 
 			---Highlight rgb colors, e.g. 'rgb(0 0 0)'
 			enable_rgb = true,
 
 			---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
-			-- enable_hsl = true,
+			enable_hsl = false,
 
 			---Highlight CSS variables, e.g. 'var(--testing-color)'
 			enable_var_usage = true,
@@ -28,7 +28,7 @@ return {
 			enable_named_colors = true,
 
 			---Highlight tailwind colors, e.g. 'bg-blue-500'
-			-- enable_tailwind = true,
+			enable_tailwind = false,
 		},
 	},
 }
