cf54a6305166c046dd20c3df451839a0e28e2e1c
amazzetta <aaron.mazzetta@gmail.com>
2025-05-09 16:15:17 +0200

    refactor: delete unused functions

--- nvim/.config/nvim/lua/user/resize_windows.lua
+++
@@ -1,8 +0,0 @@
--- nvim/lua/user/resize_windows.lua
-
-vim.api.nvim_create_autocmd("VimResized", {
-	group = vim.api.nvim_create_augroup("WinResize", { clear = true }),
-	pattern = "*",
-	command = "wincmd =",
-	desc = "Auto-resize windows on terminal buffer resize.",
-})
