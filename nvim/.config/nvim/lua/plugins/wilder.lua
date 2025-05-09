b19c3c79a99708c0d60c231201fd07a00d4aebf6
amazzetta <aaron.mazzetta@gmail.com>
2025-05-09 17:18:26 +0200

    chore: chore

--- nvim/.config/nvim/lua/plugins/wilder.lua
+++ nvim/.config/nvim/lua/plugins/wilder.lua
@@ -1,60 +1,62 @@
 -- nvim/lua/plugins/wilder.lua
 
 return {
-    {
-        "gelguy/wilder.nvim",
-        keys = {
-            ":",
-            "/",
-            "?",
-        },
-        dependencies = {
-            "catppuccin/nvim",
-        },
-        build = function() vim.fn["UpdateRemotePlugins"]() end,
-        config = function()
-            local wilder = require("wilder")
-            wilder.enable_cmdline_enter()
+	{
+		"gelguy/wilder.nvim",
+		keys = {
+			":",
+			"/",
+			"?",
+		},
+		dependencies = {
+			"catppuccin/nvim",
+		},
+		build = function()
+			vim.fn["UpdateRemotePlugins"]()
+		end,
+		config = function()
+			local wilder = require("wilder")
+			wilder.enable_cmdline_enter()
 
-            local macchiato = require("catppuccin.palettes").get_palette("macchiato")
+			local macchiato = require("catppuccin.palettes").get_palette("macchiato")
 
-            local text_highlight =
-                wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground = macchiato.text } })
-            local mauve_highlight =
-                wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = macchiato.mauve } })
+			local text_highlight =
+				wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground = macchiato.text } })
+			local mauve_highlight =
+				wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = macchiato.mauve } })
 
-            wilder.setup({ modes = { ":", "/", "?" } })
+			wilder.setup({ modes = { ":", "/", "?" } })
 
-            -- Enable fuzzy matching for commands and buffers
-            wilder.set_option("pipeline", {
-                wilder.branch(
-                    wilder.cmdline_pipeline({
-                        fuzzy = 1,
-                    }),
-                    wilder.vim_search_pipeline({
-                        fuzzy = 1,
-                    })
-                ),
-            })
+			-- Enable fuzzy matching for commands and buffers
+			wilder.set_option("pipeline", {
+				wilder.branch(
+					wilder.cmdline_pipeline({
+						fuzzy = 1,
+					}),
+					wilder.vim_search_pipeline({
+						fuzzy = 1,
+					})
+				),
+			})
 
-            wilder.set_option(
-                "renderer",
-                wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-                    highlighter = wilder.basic_highlighter(),
-                    highlights = {
-                        default = text_highlight,
-                        border = mauve_highlight,
-                        accent = mauve_highlight,
-                    },
-                    pumblend = 5,
-                    min_width = "100%",
-                    min_height = "25%",
-                    max_height = "25%",
-                    border = "rounded",
-                    left = { " ", wilder.popupmenu_devicons() },
-                    right = { " ", wilder.popupmenu_scrollbar() },
-                }))
-            )
-        end,
-    },
+			wilder.set_option(
+				"renderer",
+				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+					highlighter = wilder.basic_highlighter(),
+					highlights = {
+						default = text_highlight,
+						border = mauve_highlight,
+						accent = mauve_highlight,
+					},
+					pumblend = 5,
+					min_width = "100%",
+					min_height = "25%",
+					max_height = "25%",
+					border = "rounded",
+					left = { " ", wilder.popupmenu_devicons() },
+					right = { " ", wilder.popupmenu_scrollbar() },
+				}))
+			)
+		end,
+	},
 }
