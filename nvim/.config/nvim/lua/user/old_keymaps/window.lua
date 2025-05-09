804af41294a18ad15b30d640fd9511eb691c8ba8
amazzetta <aaron.mazzetta@gmail.com>
2025-05-09 16:14:15 +0200

    refactor: delete old keymaps

--- nvim/.config/nvim/lua/user/old_keymaps/window.lua
+++
@@ -1,7 +0,0 @@
-local nnoremap = require("user.keymaps.utils").nnoremap
-
-return function()
-    nnoremap("<leader>m", ":MaximizerToggle<cr>", { desc = "Window: Maximize" })
-    nnoremap("<leader>=", "<C-w>=", { desc = "Window: resize" })
-    nnoremap("<leader>rw", ":RotateWindows<cr>", { desc = "Window: rotate" })
-end
