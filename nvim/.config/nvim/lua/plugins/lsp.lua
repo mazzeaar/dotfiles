1be5d170fca8b52dc6cb71a863437e451171b353
amazzetta <aaron.mazzetta@gmail.com>
2025-05-15 09:41:27 +0200

    fix: make luals aware of global var 'vim'

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -72,10 +72,17 @@ M.config = function()
 			settings = {
 				Lua = {
 					runtime = { version = "LuaJIT" },
+					diagnostics = {
+						globals = {
+							"vim",
+							"require",
+						},
+					},
 					workspace = {
 						checkThirdParty = false,
 						library = {
 							"${3rd}/luv/library",
+							-- make lsp aware of neovim runtime files
 							unpack(vim.api.nvim_get_runtime_file("", true)),
 						},
 					},
