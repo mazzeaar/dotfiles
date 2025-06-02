23e03ba391b611adc7f93933075ba29a3f8f4dd4
amazzetta <aaron.mazzetta@gmail.com>
2025-06-02 12:27:49 +0200

    refactor(nvim): keymaps are now grouped and cleaned up

--- nvim/.config/nvim/lua/user/vim-keys.lua
+++ nvim/.config/nvim/lua/user/vim-keys.lua
@@ -1,136 +1,130 @@
-local nnoremap = require("user.keymap-utils").nnoremap
-local vnoremap = require("user.keymap-utils").vnoremap
-local tnoremap = require("user.keymap-utils").tnoremap
+local map_util = require("user.utils.keymap")
 
-
--- ==== Quality of life shortcuts ====
-nnoremap("<leader>w", "<cmd>w<cr>", { desc = "Quick save", silent = false })
-nnoremap("<leader>q", "<cmd>q<cr>", { desc = "Quick exit", silent = false })
-vnoremap("<leader>p", '"_dP', { desc = "Paste without losing buffer content" })
-nnoremap("U", "<C-r>", { desc = "Redo" })
-
-nnoremap("<leader>'", "<C-^>", { desc = "Switch to last buffer" })
-
-nnoremap("<leader>e",
-    function() require("oil").toggle_float() end, { desc = "Open file tree" })
-
-vnoremap("<", "<gv", { desc = "Indent left" })
-vnoremap(">", ">gv", { desc = "Indent right" })
-
-nnoremap("<leader>hs", "<cmd>split<cr>", { desc = "Horizontal split" })
-nnoremap("<leader>vs", "<cmd>vsplit<cr>", { desc = "Vertical split" })
-
-
--- ==== terminal keymaps ====
-tnoremap("<esc>", [[<C-\><C-n>]], { desc = "Terminal: normal mode" })
-tnoremap("<C-h>", [[<Cmd>wincmd h<CR>]], { desc = "Window: left" })
-tnoremap("<C-l>", [[<Cmd>wincmd l<CR>]], { desc = "Window: right" })
-tnoremap("<C-j>", [[<Cmd>wincmd j<CR>]], { desc = "Window: down" })
-tnoremap("<C-k>", [[<Cmd>wincmd k<CR>]], { desc = "Window: up" })
-
-
--- ==== TMUX keymaps ====
-nnoremap("<C-j>", function()
-    if vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
-        vim.cmd.NvimTmuxNavigateDown()
-    else
-        vim.cmd.wincmd("j")
-    end
-end)
-
-nnoremap("<C-k>", function()
-    if vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
-        vim.cmd.NvimTmuxNavigateUp()
-    else
-        vim.cmd.wincmd("k")
-    end
-end)
-
-nnoremap("<C-l>", function()
-    if vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
-        vim.cmd.NvimTmuxNavigateRight()
-    else
-        vim.cmd.wincmd("l")
-    end
-end)
-
-nnoremap("<C-h>", function()
-    if vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
-        vim.cmd.NvimTmuxNavigateLeft()
-    else
-        vim.cmd.wincmd("h")
-    end
-end)
-
+local default_map = map_util.curry_map("<leader>", "")
+local nnoremap = map_util.nnoremap
+local vnoremap = map_util.vnoremap
 
 -- ==== General keymaps =====
-nnoremap("<leader>no", ":nohlsearch<CR>",
-    { desc = "Clear search highlight ", silent = true })
+default_map("w", "<cmd>w<cr>", { desc = "Quick Save", silent = false })
+default_map("q", "<cmd>q<cr>", { desc = "Quick Quit", silent = false })
+default_map("e", function() require("oil").toggle_float() end, { desc = "File Tree" })
+
+default_map("'", "<C-^>", { desc = "Switch to Last Buffer" })
+default_map("no", ":nohlsearch<CR>", { desc = "Clear Search", silent = true })
+default_map("ut", ":UndotreeToggle<CR>", { desc = "Undo Tree" })
+
+vnoremap("<leader>p", '"_dP', { desc = "Paste without clobbering yank" })
+vnoremap("<", "<gv", { desc = "Indent Left" })
+vnoremap(">", ">gv", { desc = "Indent Right" })
+
+nnoremap("U", "<C-r>", { desc = "Redo" })
+
+default_map("sh", function()
+    vim.cmd("split")
+    vim.cmd("wincmd j")
+end, { desc = "Split Horizontal and Move" })
+
+default_map("sv", function()
+    vim.cmd("vsplit")
+    vim.cmd("wincmd l")
+end, { desc = "Split Vertical and Move" })
 
 
--- ==== Folding ====
-nnoremap("zR", "<cmd>lua require('ufo').openAllFolds()<CR>", { desc = "Open all folds" })
-nnoremap("zM", "<cmd>lua require('ufo').closeAllFolds()<CR>", { desc = "Close all folds" })
-nnoremap("K", function()
-    local winid = require("ufo").peekFoldedLinesUnderCursor()
-    if winid ~= nil then
-        vim.cmd("stopinsert")
+-- ==== terminal navigation ====
+local tnoremap = map_util.tnoremap
+
+tnoremap("<esc>", [[<C-\><C-n>]], { desc = "Exit Terminal Mode" })
+tnoremap("<C-h>", [[<Cmd>wincmd h<CR>]], { desc = "Move Left" })
+tnoremap("<C-j>", [[<Cmd>wincmd j<CR>]], { desc = "Move Down" })
+tnoremap("<C-k>", [[<Cmd>wincmd k<CR>]], { desc = "Move Up" })
+tnoremap("<C-l>", [[<Cmd>wincmd l<CR>]], { desc = "Move Right" })
+
+
+-- ==== TMUX navigation ====
+local tmux_map = map_util.curry_map("", "Tmux", "n")
+
+local function tmux_or_wincmd(cmd, fallback)
+    return function()
+        if vim.fn.exists(":" .. cmd) ~= 0 then
+            vim.cmd(cmd)
+        else
+            vim.cmd("wincmd " .. fallback)
+        end
     end
-end, { desc = "Peek folded lines" })
+end
+
+tmux_map("<C-h>", tmux_or_wincmd("NvimTmuxNavigateLeft", "h"), { desc = "Move Left" })
+tmux_map("<C-j>", tmux_or_wincmd("NvimTmuxNavigateDown", "j"), { desc = "Move Down" })
+tmux_map("<C-k>", tmux_or_wincmd("NvimTmuxNavigateUp", "k"), { desc = "Move Up" })
+tmux_map("<C-l>", tmux_or_wincmd("NvimTmuxNavigateRight", "l"), { desc = "Move Right" })
 
 
 -- ==== Harpoon ====
 local harpoon = require("harpoon")
+local list = harpoon:list()
+local harpoon_map = map_util.curry_map("<leader>h", "Harpoon")
 
-nnoremap("<leader>ho", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Open UI" })
-nnoremap("<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon: Add current file" })
-nnoremap("<leader>hd", function() harpoon:list():remove() end, { desc = "Harpoon: Remove current file" })
-nnoremap("<leader>hc", function() harpoon:list():clear() end, { desc = "Harpoon: Remove all files" })
-
-nnoremap("<leader>1", function()
-    harpoon:list():select(1)
-end, { desc = "Harpoon: jump 1" })
-
-nnoremap("<leader>2", function()
-    harpoon:list():select(2)
-end, { desc = "Harpoon: jump 2" })
-
-nnoremap("<leader>3", function()
-    harpoon:list():select(3)
-end, { desc = "Harpoon: jump 3" })
-
-nnoremap("<leader>4", function()
-    harpoon:list():select(4)
-end, { desc = "Harpoon: jump 4" })
-
-nnoremap("<leader>5", function()
-    harpoon:list():select(5)
-end, { desc = "Harpoon: jump 5" })
-
-
--- ==== Undo Tree ====
-nnoremap("<leader>ut", ":UndotreeToggle<CR>", { desc = "Toggle [U]ndo[T]ree " })
+harpoon_map("o", function() harpoon.ui:toggle_quick_menu(list) end, { desc = "Toggle UI" })
+harpoon_map("a", function() list:add() end, { desc = "Add File" })
+harpoon_map("d", function() list:remove() end, { desc = "Remove File" })
+harpoon_map("c", function() list:clear() end, { desc = "Clear List" })
+for i = 1, 5 do
+    harpoon_map(tostring(i), function() list:select(i) end, { desc = "Jump to " .. i })
+end
 
 
 -- ==== Telescope ====
 local telescope = require("telescope.builtin")
+local telescope_map = map_util.curry_map("<leader>f", "Telescope")
 
-nnoremap("<leader>fb", telescope.buffers, { desc = "Telescope: search buffers" })
-nnoremap("<leader>fg", telescope.live_grep, { desc = "Telescope: live grep" })
-nnoremap("<leader>fh", telescope.help_tags, { desc = "Telescope: help tags" })
-nnoremap("<leader>ff", telescope.find_files, { desc = "Telescope: find files" })
-nnoremap("<leader>fa", function()
-    telescope.find_files({
-        hidden = true,
-        no_ignore = true,
-        follow = true,
-    })
-end, { desc = "Telescope: find all files" })
+telescope_map("f", telescope.find_files, { desc = "Find Files" })
+telescope_map("g", telescope.live_grep, { desc = "Live Grep" })
+telescope_map("b", telescope.buffers, { desc = "Search Buffers" })
+telescope_map("h", telescope.help_tags, { desc = "Help Tags" })
+telescope_map("a", function() telescope.find_files({ hidden = true, no_ignore = true, follow = true }) end,
+    { desc = "Find All Files" })
 
 
 -- ==== Snacks ====
 local snacks = require("snacks")
+local snacks_map = map_util.curry_map("<leader>", "Snacks")
 
-nnoremap("<leader>og", function() snacks.gitbrowse() end, { desc = "[O]pen [G]it" })
-nnoremap("<leader>gb", function() snacks.git.blame_line() end, { desc = "[G]it [B]lame" })
-nnoremap("<leader>nh", function() snacks.notifier.show_history() end, { desc = "Show [N]otifier [H]istory" })
+snacks_map("og", function() snacks.gitbrowse() end, { desc = "Open Git" })
+snacks_map("gb", function() snacks.git.blame_line() end, { desc = "Git Blame Line" })
+snacks_map("nh", function() snacks.notifier.show_history() end, { desc = "Notifier History" })
+
+
+-- ==== LSP ====
+local lsp_utils = require("user.utils.lsp")
+
+local lsp_map = lsp_utils.lsp_map
+local lsp_multi_map = lsp_utils.lsp_multi_map
+local lsp_goto_map = lsp_utils.goto_map
+local jump_next = lsp_utils.jump_next
+local jump_prev = lsp_utils.jump_prev
+
+jump_next("d", "Diagnostic")
+jump_prev("d", "Diagnostic")
+jump_next("e", "Error", vim.diagnostic.severity.ERROR)
+jump_prev("e", "Error", vim.diagnostic.severity.ERROR)
+jump_next("w", "Warning", vim.diagnostic.severity.WARN)
+jump_prev("w", "Warning", vim.diagnostic.severity.WARN)
+
+lsp_map('<leader>fl', vim.diagnostic.setloclist, 'Open Quickfix list')
+lsp_map('<leader>rn', vim.lsp.buf.rename, 'Rename')
+
+lsp_goto_map('D', vim.lsp.buf.declaration, 'Declaration')
+
+lsp_multi_map('<leader>ca', vim.lsp.buf.code_action, 'Code Action')
+
+lsp_goto_map('d', telescope.lsp_definitions, 'Definition')
+lsp_goto_map('r', telescope.lsp_references, 'References')
+lsp_goto_map('I', telescope.lsp_implementations, 'Implementation')
+
+lsp_map('<leader>D', telescope.lsp_type_definitions, 'Type Definition')
+lsp_map('<leader>ds', telescope.lsp_document_symbols, 'Document Symbols')
+lsp_map('<leader>ws', telescope.lsp_dynamic_workspace_symbols, 'Workspace Symbols')
+
+
+default_map("td", function() require("user.utils.diagnostic").toggle_virtual_lines() end,
+    { desc = "Toggle Virtual Lines" })
