2c97ebf29ab4f2901ae059eae6437be065f88d74
amazzetta <aaron.mazzetta@gmail.com>
2025-06-05 11:03:19 +0200

    feat(nvim): add cmake lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls", "clangd", "pyright", "rust_analyzer", },
+            ensure_installed = { "lua_ls", "clangd", "pyright", "rust_analyzer", "cmake" },
         },
         dependencies = {
             {
