6b679e449e19c583bc8c8e429824ceff24ecdda8
amazzetta <aaron.mazzetta@gmail.com>
2025-06-07 14:12:03 +0200

    style(nvim): shorten harpoon config

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -4,9 +4,7 @@ return {
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
-            local harpoon = require("harpoon")
-
-            harpoon:setup()
+            require("harpoon"):setup()
         end
     }
 }
